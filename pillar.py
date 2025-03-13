df09a8e584bc1039b2039ee1f6af3060f3e5a252
Peter Schrimpel <peter.schrimpel10@gmail.com>
2025-03-14 00:13:30 +0100

    feat: implement scoring system (game fully functional)

--- pillar.py
+++ pillar.py
@@ -9,6 +9,7 @@ class Pillar():
     _screen: pygame.Surface | None
     _bottom_pill: pygame.Rect | None
     _top_pill: pygame.Rect | None
+    _score_collider: pygame.Rect | None
 
     def __init__(self, screen):
         self._screen = screen
@@ -18,7 +19,7 @@ class Pillar():
         bottom_pill_height = random.randint(self._screen.get_height() // 4, self._screen.get_height() // 2)
 
         self._bottom_pill = pygame.Rect((
-            self._screen.get_width() + PILLAR_WIDTH,
+            self._screen.get_width(),
             self._screen.get_height() - bottom_pill_height + BORDER_WIDTH,
             PILLAR_WIDTH,
             bottom_pill_height + BORDER_WIDTH
@@ -26,12 +27,19 @@ class Pillar():
         self._draw_half_pillar(self._bottom_pill)
 
         self._top_pill = pygame.Rect((
-            self._screen.get_width() + PILLAR_WIDTH,
+            self._screen.get_width(),
             -BORDER_WIDTH,
             PILLAR_WIDTH,
             self._screen.get_height() - bottom_pill_height - GAP_LENGTH + BORDER_WIDTH
         ))
         self._draw_half_pillar(self._top_pill)
+
+        self._score_collider = pygame.Rect((
+            self._screen.get_width() + PILLAR_WIDTH,
+            0,
+            1,
+            self._screen.get_height()
+        ))
         
     def _draw_half_pillar(self, pill):
         pygame.draw.rect(self._screen, (0, 255, 0), pill)
@@ -43,13 +51,14 @@ class Pillar():
 
         self._bottom_pill.move_ip(-speed, 0)
         self._top_pill.move_ip(-speed, 0)
+        self._score_collider.move_ip(-speed, 0)
 
-    def get_x(self):
-        return self._bottom_pill.centerx
-    
     def is_offscreen(self):
         return self._bottom_pill.right < 0
     
     def is_collision(self, other: pygame.Rect):
         return self._bottom_pill.colliderect(other) or self._top_pill.colliderect(other)
+    
+    def is_score(self, other: pygame.Rect):
+        return self._score_collider.colliderect(other)
 
